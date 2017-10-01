655436f2bdb9e8ef64b3d6831d8764db0185fb50
JF-Lalonde <lalondej88@gmail.com>
2017-10-01 15:58:22 -0600

    Add two more tests for bubble sort

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -1,11 +1,20 @@
-const assert = require('chai').assert
+const assert = require('chai').assert;
 const bubbleSort = require('../bubbleSort')
 
 describe('sorting functionality', function(){
   context('sorting function', function(){
+    var testArray = [4,2,3,1];
     it('it can sort an array', function(){
-      var testArray = [4,2,3,1];
       assert.deepEqual(bubbleSort(testArray), [1,2,3,4]);
+    });
+
+    it('it returns an array', function(){
+      assert.isArray(bubbleSort(testArray));
+    });
+
+    it('it can sort arrays of different sizes', function(){
+      var newArray = [8,9,4,3,2,0]
+      assert.deepEqual(bubbleSort(newArray), [0,2,3,4,8,9]);
     });
   });
 });
